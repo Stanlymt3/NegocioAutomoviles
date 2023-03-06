37d8c3568437347494df2e06e247d0610521b82b
pizzaroni <stanlymt3zx@gmail.com>
2023-03-05 22:10:10 -0600

    Entity Classes

--- src/main/java/com/pizzaroni/NegocioAutomoviles/Entity/Vehiculos.java
+++ src/main/java/com/pizzaroni/NegocioAutomoviles/Entity/Vehiculos.java
@@ -1,4 +1,81 @@
 package com.pizzaroni.NegocioAutomoviles.Entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "vehiculos")
 public class Vehiculos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "marca")
+    private String marca;
+
+    @Column(name = "modelo")
+    private String modelo;
+
+    @Column(name = "anio")
+    private int anio;
+
+    @Column(name = "precio")
+    private BigDecimal precio;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    //Getters and Setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- src/main/java/com/pizzaroni/NegocioAutomoviles/Entity/Ventas.java
+++ src/main/java/com/pizzaroni/NegocioAutomoviles/Entity/Ventas.java
@@ -1,4 +1,73 @@
 package com.pizzaroni.NegocioAutomoviles.Entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ventas")
 public class Ventas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Clientes cliente_id;
+
+    @ManyToOne
+    @JoinColumn(name = "vehiculo_id")
+    private Vehiculos vehiculo_id;
+
+    @Column(name = "fecha_venta")
+    private LocalDate fecha_venta;
+
+    @Column(name = "precio_venta")
+    private BigDecimal precio_venta;
+
+    //Getters and Setters
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Clientes getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(Clientes cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public Vehiculos getVehiculo_id() {
+        return vehiculo_id;
+    }
+
+    public void setVehiculo_id(Vehiculos vehiculo_id) {
+        this.vehiculo_id = vehiculo_id;
+    }
+
+    public LocalDate getFecha_venta() {
+        return fecha_venta;
+    }
+
+    public void setFecha_venta(LocalDate fecha_venta) {
+        this.fecha_venta = fecha_venta;
+    }
+
+    public BigDecimal getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(BigDecimal precio_venta) {
+        this.precio_venta = precio_venta;
+    }
 }
