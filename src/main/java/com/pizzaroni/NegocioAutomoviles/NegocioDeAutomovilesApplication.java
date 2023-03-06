0330bb90c5786a945eb9cdf901b0fb6af8295112
pizzaroni <stanlymt3zx@gmail.com>
2023-03-05 21:11:31 -0600

    Initial commit

---
+++ src/main/java/com/pizzaroni/NegocioAutomoviles/NegocioDeAutomovilesApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaroni.NegocioAutomoviles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioDeAutomovilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegocioDeAutomovilesApplication.class, args);
+	}
+
+}
